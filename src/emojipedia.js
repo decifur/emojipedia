9b662ef471aff6711d37fc595223753dd4a1203e
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-09-06 21:35:16 +0530

    added some more emojis

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -46,6 +46,30 @@ const emojipedia = [
     name: "Face with spiral eyes",
     meaning:
       "The mouth is open, the eyes are spirals. The emoji has circulatory problems and is dizzy. Something or someone has cast a spell over us, we are hypnotized. There may be disorientation, e.g. in a foreign city or in a department store."
+  },
+  {
+    id: 7,
+    emoji:
+      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Biting%20Lip.png",
+    name: "Biting Lip",
+    meaning:
+      "The bite on the lip signals: I find you hot and want to flirt! But it can also be used as a symbol of anxiety, tension or insecurity. Ask your conversation partner about his thoughts."
+  },
+  {
+    id: 8,
+    emoji:
+      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Exploding%20Head.png",
+    name: "Exploding Head",
+    meaning:
+      "I cannot believe it. or “That will blow you away.“ Something is very exciting or surprising. The exploding head can also be used to express shock or awe."
+  },
+  {
+    id: 6,
+    emoji:
+      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Face%20in%20Clouds.png",
+    name: "Face in clouds",
+    meaning:
+      "This is nebulous! The face is surrounded by fog or clouds. Those who have their heads in the clouds are either dreamy or do not want to admit reality. The view of something is blurred. There may also be a state of confusion or bliss."
   }
 ];
 
